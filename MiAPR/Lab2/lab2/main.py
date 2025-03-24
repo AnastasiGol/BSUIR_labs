1dab5209a517bd4ac5337f8c034d4e1085eb10b2
anast <anastasia.gol.ubeva@inbox.ru>
2025-03-24 14:51:57 +0300

    MiAPR updated

--- MiAPR/Lab2/lab2/main.py
+++ MiAPR/Lab2/lab2/main.py
@@ -14,14 +14,15 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
 def euclidean_distance(x1, x2):
-    return np.sqrt(np.sum((x1 - x2) ** 2))
+    return np.linalg.norm(x1 - x2)     #np.sqrt(np.sum((x1 - x2) ** 2))
+
 
 
 #каждой точке определяю новый класс по центроиде
-def class_list(X, cores):
+def update_class_list(X, cores):
     new_classes = np.zeros(len(X))
     for i in range(len(X)):
-        min_dist = 100000
+        min_dist = 100000000
         for j in range(len(cores)):
             dist = euclidean_distance(X[i], cores[j])
             if dist < min_dist:
@@ -54,38 +55,41 @@ def find_max_distances(X, cores, classes):
 
 
 def mean_core_distance(cores):
-    """Считает среднее арифметическое расстояний между всеми ядрами."""
+    # среднее арифметическое расстояний между всеми ядрами
     distances = [np.linalg.norm(np.array(c1) - np.array(c2)) for c1, c2 in combinations(cores, 2)]
     return np.mean(distances)
 
 
 def max_algorithm(n_samples, widget):
-    X = np.random.randint(0, 100_000, size=(n_samples, 2))
-    cores = []
+    #np.random.seed(101)
+    X = np.random.randint(0, 100_000, size=(n_samples, 2),)
+    cores = np.empty((0, 2), dtype=int)
     classes = np.zeros(len(X))
 
     n1 = X[np.random.randint(0, len(X))]
-    cores.append(n1)
+    cores = np.vstack((cores, n1))
     n2 = X[find_max_index(X, n1)]
-    cores.append(n2)
-    print(X)
-    print(cores)
+    cores = np.vstack((cores, n2))
 
 
     can_add_new = True
     while can_add_new:
-        classes = class_list(X, cores)
+        classes = update_class_list(X, cores)
+
         max_distances, max_indexes = find_max_distances(X, cores, classes)
         mean_distance = mean_core_distance(cores)
         if max(max_distances) > mean_distance / 2:   #самая большое расстояние больше чем половина между всеми ядрами
             index = np.argmax(max_distances)
             core = X[max_indexes[index]]
-            cores.append(core)
+            cores = np.vstack((cores, core))
             print(f"cores length: {len(cores)}")
             print(f"cores: {cores}")
         else:
             can_add_new = False
-    Visualiser(X, np.array(cores), classes, widget)
+
+    Visualiser(X, cores, classes, widget)
+
+
 
 class Canvas(FigureCanvas):
     def __init__(self, width=5, height=4, dpi=100):
@@ -104,8 +108,15 @@ class Visualiser(QMainWindow):
         self.layout = QVBoxLayout()
         self.layout.addWidget(canvas)
         widget.setLayout(self.layout)
-        self.show()
+        #self.show()
 
+
+def is_valid_number(textEdit, min_value, max_value):
+    try:
+        number = int(textEdit.toPlainText().strip())
+        return min_value <= number <= max_value
+    except ValueError:
+        return False
 
 
 class App(QMainWindow):
@@ -118,39 +129,15 @@ class App(QMainWindow):
 
         self.canvas = Canvas()
 
-
     def validate_input(self):
-        n_valid = self.is_valid_number(self.NEdit, 1000, 10_000)
+        n_valid = is_valid_number(self.NEdit, 1000, 100_000)
         self.calculateButton.setEnabled( n_valid)
-
-    def is_valid_number(self, textEdit, min_value, max_value):
-        try:
-            number = int(textEdit.toPlainText().strip())
-            return min_value <= number <= max_value
-        except ValueError:
-            return False
 
     def calculate(self):
         n_samples = int(self.NEdit.toPlainText())
 
-        X = np.random.randint(0, 100_000, size=(n_samples, 2))
-        cores = []
-        classes = np.zeros(len(X))
-
-        n1 = X[np.random.randint(0, len(X))]
-        cores.append(n1)
-        n2 = X[find_max_index(X, n1)]
-        cores.append(n2)
-        print(X)
-        print(cores)
         max_algorithm(n_samples, self.widget)
-'''
-        self.canvas.axes.scatter(X[:, 0], X[:, 1], c=classes, cmap="viridis", alpha=0.7, s=5, label="Образы")
-        self.canvas.axes.scatter(cores[:, 0], cores[:, 1], s=150, c='r', marker='*')
-        layout = QVBoxLayout()
-        layout.addWidget(self.canvas)# а какого хуя
-        self.widget.setLayout(layout)
-'''
+
 
 
 
